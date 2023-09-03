4ea76b5c1bd1147241e35392990f5cd4adf8e1e0
nooralu <nooralu@foxmail.com>
2023-09-03 11:11:04 +0800

    format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use cli::{Arguments, Cli};
 use clap::Parser;
-use sniffer::scan_ports_multi_threads;
+use cli::{Arguments, Cli};
 use colored::Colorize;
+use sniffer::scan_ports_multi_threads;
 
 mod cli;
 mod sniffer;
@@ -10,17 +10,28 @@ mod sniffer;
 async fn main() -> Result<(), String> {
     let app = Cli::parse();
     let args = Arguments::new(&app)?;
-    println!("{}", format!("Scanning port(s) on {} ({})", app.destination, args.ip).yellow());
+    println!(
+        "{}",
+        format!("Scanning port(s) on {} ({})", app.destination, args.ip).yellow()
+    );
 
     let open_ports =
         scan_ports_multi_threads(args.ip, &args.ports, args.timeout, args.num_threads).await;
 
-    println!("\n{}", format!("{} port(s) scanned, {} open port(s)\n", args.ports.len(), open_ports.len()).yellow());
+    println!(
+        "\n{}",
+        format!(
+            "{} port(s) scanned, {} open port(s)\n",
+            args.ports.len(),
+            open_ports.len()
+        )
+        .yellow()
+    );
 
     for port in &open_ports {
         println!("{}", format!("{}\tOPEN", port).green());
     }
-    
+
     for port in &args.ports {
         if !open_ports.contains(port) {
             println!("{}", format!("{}\tCLOSED", port).red());
