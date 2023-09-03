6839a3be335bc66721a0d317df2d436a5d004d7b
nooralu <nooralu@foxmail.com>
2023-09-03 10:21:55 +0800

    Support port ranges

--- src/cli.rs
+++ src/cli.rs
@@ -39,16 +39,7 @@ impl Arguments {
     pub fn new() -> Result<Self, String> {
         let app = Cli::parse();
         let ip = IpAddr::from_str(&app.ip).map_err(|_| "Invalid IP address")?;
-        // TODO: support port ranges, e.g. 80-90
-        let ports = app
-            .ports
-            .split(',')
-            .filter(|port| !port.is_empty())
-            .map(|port| {
-                port.parse::<u16>()
-                    .map_err(|_| format!("Invalid port: {}", port))
-            })
-            .collect::<Result<Vec<u16>, String>>()?;
+        let ports = parse_ports(&app.ports)?;
         let num_threads = app.threads.unwrap_or(5);
         let timeout = Duration::from_millis(app.timeout.unwrap_or(500));
         Ok(Arguments {
@@ -58,4 +49,35 @@ impl Arguments {
             timeout,
         })
     }
+}
+
+fn parse_ports(args: &str) -> Result<Vec<u16>, String> {
+    let mut ports = vec![];
+    for arg in args.split(',').filter(|arg| !arg.is_empty()) {
+        if arg.contains('-') {
+            let range = arg
+                .split('-')
+                .map(|port| port.parse::<u16>())
+                .collect::<Result<Vec<u16>, _>>()
+                .map_err(|_| format!("Invalid port range: {}", arg))?;
+            if range.len() != 2 {
+                return Err(format!("Invalid port range: {}", arg));
+            }
+            let start = range[0];
+            let end = range[1];
+            if start > end {
+                return Err(format!("Invalid port range: {}", arg));
+            }
+            ports.extend(start..=end);
+        } else {
+            let port = arg
+                .parse::<u16>()
+                .map_err(|_| format!("Invalid port: {}", arg));
+            ports.push(port?);
+        }
+    }
+    // Remove duplicates
+    ports.sort_unstable();
+    ports.dedup();
+    Ok(ports)
 }
